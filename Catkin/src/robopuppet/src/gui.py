12e1bac4086bb7a71760cbc08b51eef340d8965b
Dan Oates <doates625@gmail.com>
2019-12-04 10:55:53 -0500

    Single-tab GUI refactor

--- Catkin/src/robopuppet/src/gui.py
+++ Catkin/src/robopuppet/src/gui.py
@@ -10,6 +10,7 @@ import rospy
 import constants
 from constants import opmode_names
 from constants import num_joints
+from constants import num_grippers
 from interface import Interface
 from Tkinter import *
 from threading import Thread
@@ -34,86 +35,216 @@ class GUI:
 		side = 'L' # rospy.get_param('~side')
 		# puppet = Interface(side)
 		
-		# Tkinter Window
-		self._root = Tk()
-		self._root.title('RoboPuppet Arm ' + side)
-		self._fr_root = Frame(self._root, width=800, height=400)
-		self._fr_root.pack_propagate(False)
-		self._fr_root.pack()
+		# Tkinter Root
+		self._root = self._make_root(side)
+		self._fr_root = self._make_fr_root(self._root)
 		
-		# Top Frame
-		self._fr_top = Frame(self._fr_root, height=50, bg='black')
-		self._fr_top.pack_propagate(False)
-		self._fr_top.pack(side='top', fill=X)
+		# Heartbeat GUI
+		self._fr_top = self._make_fr_top(self._fr_root)
+		self._fr_hb = self._make_fr_hb(self._fr_top)
+		self._lb_hb = self._make_lb_hb(self._fr_hb)
 		
-		# Heartbeat Frame
-		self._fr_hb = Frame(self._fr_top, bg='blue')
-		self._fr_hb.pack_propagate(False)
-		self._fr_hb.pack(side='left', expand=True, fill=BOTH)
+		# Opmode GUI
+		self._fr_om = self._make_fr_om(self._fr_top)
+		self._lb_om = self._make_lb_om(self._fr_om)
+		self._sv_om = self._make_rbs_om(self._fr_om)
 		
-		# Heartbeat Label
-		self._lb_hb = Label(self._fr_hb, text=('Last Heartbeat: %.2f' % 0.0))
-		self._lb_hb.pack(expand=True)
+		# Joint State GUI
+		self._lb_js = self._make_lb_js(self._fr_root)
+		self._fr_js = self._make_fr_js(self._fr_root)
+		self._lbs_js = self._make_lbs_js(self._fr_js)
 		
-		# Opmode Frame
-		self._fr_om = Frame(self._fr_top, bg='red')
-		self._fr_om.pack_propagate(False)
-		self._fr_om.pack(side='right', expand=True, fill=BOTH)
-		
-		# Opmode Label
-		self._lb_om = Label(self._fr_om, text='Opmode')
-		self._lb_om.pack(side='top')
-		
-		# Opmode Buttons
-		self._sv_om = StringVar()
-		self._sv_om.set(opmode_names[0])
-		self._bt_om = dict()
-		for name in opmode_names:
-			self._bt_om[name] = Radiobutton(
-				self._fr_om, text=name, variable=self._sv_om, value=name, indicatoron=0)
-			self._bt_om[name].pack(side='left', expand=True, fill=BOTH)
-			
-		# Joint State Label
-		self._lb_js = Label(self._fr_root, text='Joint States')
-		self._lb_js.pack(side='top')
-		
-		# Joint State Frame
-		self._fr_js = Frame(self._fr_root, bg='purple')
-		self._fr_js.pack_propagate(False)
-		self._fr_js.pack(side='top', expand=True, fill=BOTH)
-		
-		# Joint State Table Labels
-		self._lb_ji = Label(self._fr_js, text='Joint Index')
-		self._lb_jc = Label(self._fr_js, text='Calibration')
-		self._lb_ja = Label(self._fr_js, text='Angle [rad]')
-		self._lb_jv = Label(self._fr_js, text='Voltage [V]')
-		self._lb_ji.grid(row=0, column=0, sticky=N+S+E+W)
-		self._lb_jc.grid(row=0, column=1, sticky=N+S+E+W)
-		self._lb_ja.grid(row=0, column=2, sticky=N+S+E+W)
-		self._lb_jv.grid(row=0, column=3, sticky=N+S+E+W)
-		for x in range(4):
-			Grid.columnconfigure(self._fr_js, x, weight=1)
-		
-		# Joint State Table Values
-		self._lb_jis = []
-		self._lb_jcs = []
-		self._lb_jas = []
-		self._lb_jvs = []
-		for j in range(num_joints):
-			self._lb_jis.append(Label(self._fr_js, text=('%u' % j)))
-			self._lb_jcs.append(Label(self._fr_js, text='False'))
-			self._lb_jas.append(Label(self._fr_js, text='0.00'))
-			self._lb_jvs.append(Label(self._fr_js, text='0.00'))
-			self._lb_jis[j].grid(row=j+1, column=0, sticky=N+S+E+W)
-			self._lb_jcs[j].grid(row=j+1, column=1, sticky=N+S+E+W)
-			self._lb_jas[j].grid(row=j+1, column=2, sticky=N+S+E+W)
-			self._lb_jvs[j].grid(row=j+1, column=3, sticky=N+S+E+W)
+		# Gripper State GUI
+		self._lb_gs = self._make_lb_gs(self._fr_root)
+		self._fr_gs = self._make_fr_gs(self._fr_root)
+		self._lbs_gs = self._make_lbs_gs(self._fr_gs)
 		
 		# Start Tkinter
 		self._update_ms = int(1000 / UPDATE_RATE)
 		self._root.after(self._update_ms, self._update)
 		self._root.mainloop()
 	
+	def _make_root(self, side):
+		"""
+		Creates, inits, and returns Tkinter root
+		:param side: Puppet arm side ['L', 'R']
+		"""
+		root = Tk()
+		root.title('RoboPuppet Arm ' + side)
+		return root
+	
+	def _make_fr_root(self, parent):
+		"""
+		Creates and returns root frame
+		:param parent: Parent frame (root)
+		"""
+		frame = Frame(parent, width=600, height=300)
+		frame.pack_propagate(False)
+		frame.pack()
+		return frame
+	
+	def _make_fr_top(self, parent):
+		"""
+		Creates and returns top frame
+		:param parent: Parent frame
+		"""
+		frame = Frame(parent, height=50)
+		frame.pack_propagate(False)
+		frame.pack(side='top', fill=X)
+		return frame
+	
+	def _make_fr_hb(self, parent):
+		"""
+		Creates and returns heartbeat frame
+		:param parent: Parent frame
+		"""
+		frame = Frame(parent)
+		frame.pack_propagate(False)
+		frame.pack(side='left', expand=True, fill=BOTH)
+		return frame
+	
+	def _make_lb_hb(self, parent):
+		"""
+		Creates and returns heartbeat label
+		:param parent: Parent frame
+		"""
+		label = Label(parent, text=('Last Heartbeat: %.2f' % 0.0))
+		label.pack(expand=True)
+		return label
+	
+	def _make_fr_om(self, parent):
+		"""
+		Creates and returns opmode frame
+		:param parent: Parent frame
+		"""
+		frame = Frame(parent)
+		frame.pack_propagate(False)
+		frame.pack(side='right', expand=True, fill=BOTH)
+		return frame
+	
+	def _make_lb_om(self, parent):
+		"""
+		Creates and returns opmode label
+		:param parent: Parent frame
+		"""
+		label = Label(parent, text='Opmode')
+		label.pack(side='top')
+		return label
+	
+	def _make_rbs_om(self, parent):
+		"""
+		Creates opmode ratio button array
+		:param parent: Parent frame
+		:return: Button state [StringVar]
+		"""
+		opmode = StringVar()
+		opmode.set(opmode_names[0])
+		self._rbs_om = dict()
+		for name in opmode_names:
+			self._rbs_om[name] = Radiobutton(
+				parent, text=name, variable=opmode, value=name, indicatoron=0)
+			self._rbs_om[name].pack(side='left', expand=True, fill=BOTH)
+		return opmode
+	
+	def _make_lb_js(self, parent):
+		"""
+		Creates and returns joint state label
+		:param parent: Parent frame
+		"""
+		label = Label(parent, text='Joint States')
+		label.pack(side='top')
+		return label
+	
+	def _make_fr_js(self, parent):
+		"""
+		Creates and returns joint state frame
+		:param parent: Parent frame
+		"""
+		frame = Frame(parent)
+		frame.pack_propagate(False)
+		frame.pack(side='top', fill=X)
+		return frame
+	
+	def _make_lbs_js(self, parent):
+		"""
+		Creates and returns joint state label nested dictionary
+		:param parent: Parent frame
+		
+		Dictionary structure:
+		labels['title'][0] = Index title [string]
+		labels['title'][1] = Calibration title [string]
+		labels['title'][2] = Angle title [string]
+		labels['title'][3] = Voltage title [string]
+		labels[0...6]['index'] = Joint index ['0'...'6']
+		labels[0...6]['cal'] = Calibration status ['True', 'False']
+		labels[0...6]['angle'] = Angle ['%+.2f', rad]
+		labels[0...6]['voltage'] = Voltage ['%+.2f', V]
+		"""
+		
+		# Create dictionary
+		labels = dict()
+		
+		# Column Titles
+		titles = ['Joint Index', 'Calibration', 'Angle [rad]', 'Voltage [V]']
+		labels['title'] = dict()
+		for (t, title) in enumerate(titles):
+			label = Label(parent, text=titles[t])
+			label.grid(row=0, column=t, sticky=N+S+E+W)
+			labels['title'][t] = label
+			Grid.columnconfigure(parent, t, weight=1)
+		
+		# Joint States
+		keys = ['index', 'cal', 'angle', 'voltage']
+		for j in range(num_joints):
+			labels[j] = dict()
+			labels[j][keys[0]] = Label(parent, text=('%u' % j))
+			labels[j][keys[1]] = Label(parent, text='False')
+			labels[j][keys[2]] = Label(parent, text=('%+.2f' % 0.00))
+			labels[j][keys[3]] = Label(parent, text=('%+.2f' % 0.00))
+			for (t, key) in enumerate(keys):
+				labels[j][key].grid(row=j+1, column=t, sticky=N+S+E+W)
+		
+		# Return dictionary
+		return labels
+	
+	def _make_lb_gs(self, parent):
+		"""
+		Creates and returns gripper state label
+		:param parent: Parent frame
+		"""
+		label = Label(parent, text='Gripper States')
+		label.pack(side='top')
+		return label
+	
+	def _make_fr_gs(self, parent):
+		"""
+		Creates and returns gripper state frame
+		:param parent: Parent frame
+		"""
+		frame = Frame(parent)
+		frame.pack_propagate(False)
+		frame.pack(side='top', fill=X)
+		return frame
+	
+	def _make_lbs_gs(self, parent):
+		"""
+		Creates and returns gripper state label nested dictionary
+		:param parent: Parent frame
+		
+		Dictionary structure:
+		labels[0...3]['title'] = Title label ['Gripper X']
+		labels[0...3]['value'] = Value label ['%+.2f', range 0-1]
+		"""
+		labels = dict()
+		for g in range(num_grippers):
+			labels[g] = dict()
+			labels[g]['title'] = Label(parent, text=('Gripper %u' % g))
+			labels[g]['value'] = Label(parent, text=('%.2f' % 0.00))
+			labels[g]['title'].grid(row=0, column=g, sticky=N+S+E+W)
+			labels[g]['value'].grid(row=1, column=g, sticky=N+S+E+W)
+			Grid.columnconfigure(parent, g, weight=1)
+		return labels
+
 	def _update(self):
 		"""
 		Updates GUI with RoboPuppet data
