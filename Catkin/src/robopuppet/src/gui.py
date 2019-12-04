e5d1290e2abffca2f4914a065028dfc2b6472fec
Dan Oates <doates625@gmail.com>
2019-12-04 17:51:18 -0500

    Added empty plots for each joint tab

--- Catkin/src/robopuppet/src/gui.py
+++ Catkin/src/robopuppet/src/gui.py
@@ -14,7 +14,14 @@ from constants import num_grippers
 from interface import Interface
 from Tkinter import *
 from ttk import Notebook
-from threading import Thread
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+from math import pi
+from time import time
 
 """
 Constants
@@ -251,6 +258,63 @@ class JointTab:
 		self._fr = Frame(parent)
 		self._fr.pack()
 		parent.add(self._fr, text=('Joint %u' % self._joint))
+		
+		# Joint and voltage plots
+		self._make_jp(self._fr)
+		self._make_vp(self._fr)
+		
+		# Config frame
+		# TODO
+	
+	def _make_jp(self, parent):
+		"""
+		Creates joint angle plot
+		:param parent: Parent frame
+		"""
+		
+		# Create plot
+		self._fig_jp = Figure(figsize=(10, 4))
+		self._axs_jp = self._fig_jp.add_subplot(1, 1, 1)
+		self._axs_jp.set_title('Joint Angle')
+		self._axs_jp.set_xlabel('Time [s]')
+		self._axs_jp.set_ylabel('Angle [rad]')
+		self._axs_jp.set_xlim(-5.0, +0.0)
+		self._axs_jp.set_ylim(-3.2, +3.2)
+		self._axs_jp.grid()
+		self._fig_jp.tight_layout()
+		
+		# Create canvas
+		self._cv_jp = FigureCanvasTkAgg(self._fig_jp, master=parent)
+		self._cv_jp.get_tk_widget().pack(side='top', fill='both')
+		self._cv_jp.draw()
+		
+		# TODO return
+		return None
+	
+	def _make_vp(self, parent):
+		"""
+		Creates joint voltage plot
+		:param parent: Parent frame
+		"""
+		
+		# Create plot
+		self._fig_vp = Figure(figsize=(10, 4))
+		self._axs_vp = self._fig_vp.add_subplot(1, 1, 1)
+		self._axs_vp.set_title('Joint Voltage')
+		self._axs_vp.set_xlabel('Time [s]')
+		self._axs_vp.set_ylabel('Voltage [V]')
+		self._axs_vp.set_xlim(-5.0, +0.0)
+		self._axs_vp.set_ylim(-7.2, +7.2)
+		self._axs_vp.grid()
+		self._fig_vp.tight_layout()
+		
+		# Create canvas
+		self._cv_vp = FigureCanvasTkAgg(self._fig_vp, master=parent)
+		self._cv_vp.get_tk_widget().pack(side='top', fill='both')
+		self._cv_vp.draw()
+		
+		# TODO return
+		return None
 	
 	def update(self):
 		"""
@@ -277,8 +341,8 @@ class GUI:
 		# Tkinter Root
 		self._root = Tk()
 		self._root.title('RoboPuppet Arm ' + side)
-		self._fr_root = Frame(self._root, width=600, height=300)
-		self._fr_root.pack_propagate(False)
+		self._fr_root = Frame(self._root, width=800, height=800)
+		# self._fr_root.pack_propagate(False)
 		self._fr_root.pack()
 		self._nb = Notebook(self._fr_root)
 		self._nb.pack(expand=True, fill='both')
